1960c6a10ead033c258ab59549144ebfc6265872
Daniel Vargas <dlvargas@stanford.edu>
2018-06-20 17:39:31 -0700

    * Added init.sh Docker entrypoint script * Added Cloudera CDH5 repository and Hadoop HDFS FUSE module

--- Dockerfile
+++ Dockerfile
@@ -1,27 +1,41 @@
-FROM centos:centos7
+FROM centos:latest
 
-RUN yum install -y java-1.8.0-openjdk-devel unzip
+MAINTAINER "Daniel Vargas" <dlvargas@stanford.edu>
 
+# Set container entrypoint
+ENTRYPOINT ["/init.sh"]
+
+# Install Cloudera CDH5 RPM repository
+RUN rpm --import https://archive.cloudera.com/cdh5/redhat/7/x86_64/cdh/RPM-GPG-KEY-cloudera
+# RUN curl -o /etc/yum.repos.d/cloudera-cdh5.repo https://archive.cloudera.com/cdh5/redhat/7/x86_64/cdh/cloudera-cdh5.repo
+# ADD https://archive.cloudera.com/cdh5/redhat/7/x86_64/cdh/cloudera-cdh5.repo /etc/yum.repos.d/cloudera-cdh5.repo
+ADD cloudera/cloudera-cdh5.repo /etc/yum.repos.d/cloudera-cdh5.repo
+
+# Install and update packages
+RUN yum -y install hadoop-hdfs-fuse java-1.8.0-openjdk-devel unzip
+RUN yum -y update && yum clean all && rm -rf /var/cache/yum
+
+# Install Gradle
 ADD https://services.gradle.org/distributions/gradle-2.13-all.zip ./gradle-2.13-all.zip
 RUN mkdir -p /opt/gradle
 RUN unzip ./gradle-2.13-all.zip -d /opt/gradle
 
+# Set environment variables
 ENV GRADLE_HOME /opt/gradle/gradle-2.13
-
 ENV PATH $PATH:/opt/gradle/gradle-2.13/bin:/usr/lib/jvm/jre/bin
-
 ENV JAVA_HOME /usr/lib/jvm/jre
 
+# Make EDINA indexer base directory
 RUN mkdir /lockss-solr
+WORKDIR /lockss-solr
 
+# Add EDINA indexer source
 ADD ./src /lockss-solr/src
 ADD ./*.gradle /lockss-solr/
 ADD ./gradle /lockss-solr/
 
-WORKDIR /lockss-solr
-
+# Build EDINA indexer JAR
 RUN gradle fatJar
 
-USER nobody
-
-CMD ["java", "-jar", "/lockss-solr/build/libs/lockss-solr-all-1.0-SNAPSHOT.jar"]
+# Add entrypoint shell script
+ADD scripts/init.sh /init.sh
---
+++ scripts/init.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+
+# Copyright (c) 2000-2018, Board of Trustees of Leland Stanford Jr. University
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without modification,
+# are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice, this
+# list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+# this list of conditions and the following disclaimer in the documentation and/or
+# other materials provided with the distribution.
+#
+# 3. Neither the name of the copyright holder nor the names of its contributors
+# may be used to endorse or promote products derived from this software without
+# specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+# ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+# ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+# Mount a HDFS path if one was provided
+if [ ! -z ${HDFS_HOST} ]; then
+  echo "Mounting HDFS within EDINA indexer (hdfs://${HDFS_HOST:-localhost}:${HDFS_FSMD:-9000}/ -> ${LOCKSS_SOLR_HDFSMNT})"
+
+  # Wait for HDFS to become available
+  while ! nc -z ${HDFS_HOST:-localhost} ${HDFS_FSMD:-9000} ; do
+      echo "Could not connect to hdfs://${HDFS_HOST:-localhost}:${HDFS_FSMD:-9000}/; retrying in 5 seconds..."
+      sleep 5
+  done
+
+  # Create mount point
+  mkdir -p ${LOCKSS_SOLR_HDFSMNT}
+
+  # Attempt to mount HDFS sealed WARCs to OpenWayback watch directory
+  hadoop-fuse-dfs "dfs://${HDFS_HOST:-localhost}:${HDFS_FSMD:-9000}/" ${LOCKSS_SOLR_HDFSMNT}
+
+  # Ensure the sealed directory exists (-p doesn't work - only used here so that mkdir is quiet if the directory already exists)
+  mkdir -p ${LOCKSS_SOLR_HDFSMNT}/${REPO_BASEDIR}
+  mkdir -p ${LOCKSS_SOLR_HDFSMNT}/${REPO_BASEDIR}/sealed
+fi
+
+# Start the EDINA indexer
+java -jar /lockss-solr/build/libs/lockss-solr-all-1.0-SNAPSHOT.jar
